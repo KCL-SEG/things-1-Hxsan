60fa17e30a5d8055e11de816277d79028d931f7c
Hasan <mohammed.6.ahmed@kcl.ac.uk>
2023-12-08 12:37:54 +0000

    Exercise 3

--- things/models.py
+++ things/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+
+def validate_quantity_range(value):
+    if not(0 <= value <=100):
+        raise ValidationError("Quantity must be between 0 and 100")
 
 # Create your models here.
 class Thing(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    quantity = models.IntegerField()
+    name = models.CharField(max_length=30, unique=True, blank=False)
+    description = models.TextField(blank=True, max_length=120)
+    quantity = models.IntegerField(validators=[validate_quantity_range])
 
     def __str__(self):
         return self.name
